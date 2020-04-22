ddaac72919b9a4d6c6195307af4d5cb711447066
JeromeTan1029 <64115240+JeromeTan1029@users.noreply.github.com>
2020-04-22 11:07:16 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -10,7 +10,22 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
+    boolean validation;
+
+    validation = false;
+    do {
+      System.out.print("Enter an integer: ");
+      Scanner input = new Scanner(System.in);
+      if (input.hasNextInt()){
+        int integer = input.nextInt();
+        System.out.println("Output: " + (integer * integer));
+        validation = true;
+      }
+      else{
+        System.out.println("Invalid Input");
+      }
+    }
+    while (validation == false);
     
   }
 }
