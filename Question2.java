f5fd78e17d21c814224f4df33309fabf43ecdfd5
JeromeTan1029 <64115240+JeromeTan1029@users.noreply.github.com>
2020-04-22 10:39:05 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,7 +10,40 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
      
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+    double weight = 0;
+    double height = 0;
+    boolean validation;
+
+    validation = false;
+    do {
+      System.out.print("Enter your height (m): ");
+      Scanner heightInput = new Scanner(System.in);
+      if (heightInput.hasNextDouble()){
+        height = heightInput.nextDouble();
+        validation = true;
+      }
+      else{
+        System.out.println("Invalid Input");
+      }
+    }
+    while (validation == false);
+
+
+    validation = false;
+    do {
+      System.out.print("Enter your weight (kg): ");
+      Scanner weightInput = new Scanner(System.in);
+      if (weightInput.hasNextDouble()){
+         weight = weightInput.nextDouble();
+         validation = true;
+      }
+      else{
+        System.out.println("Invalid Input");
+      }
+    }
+    while (validation == false);
+    
+    double bmi = weight / (height * height);
+    System.out.println("Your BMI is: " + bmi);
   }
 }
